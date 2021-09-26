8f0dc8226957e678495437af4d34442818067670
Francoco97 <fpalmatrejo@gmail.com>
2021-09-26 20:53:54 -0300

    asd

--- frontend/src/layouts/authentication/sign-in/index.js
+++ frontend/src/layouts/authentication/sign-in/index.js
@@ -23,6 +23,7 @@ const hist = createBrowserHistory();
 const Inicio = () => (
   <Router history={hist}>
     <Switch>
+      <Route path="/admin"/>
       <Redirect from="/authentication/sign-in" to="/dashboard" />
     </Switch>
   </Router>
