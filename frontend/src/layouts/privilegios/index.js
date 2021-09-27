e227adad90c9dd446fc5504160a609552ea46f92
Francoco97 <fpalmatrejo@gmail.com>
2021-09-27 17:08:31 -0300

    creación pagina en blanco

---
+++ frontend/src/layouts/privilegios/index.js
@@ -0,0 +1,32 @@
+
+import Card from "@material-ui/core/Card";
+
+// Soft UI Dashboard Material-UI components
+import SuiBox from "components/SuiBox";
+import SuiTypography from "components/SuiTypography";
+
+// Soft UI Dashboard Material-UI example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import Table from "examples/Table";
+
+function Privilegios() {
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <SuiBox py={3}>
+        <SuiBox mb={3}>
+          <Card>
+            <SuiBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
+              <SuiTypography variant="h6">Tabla de Privilegios</SuiTypography>
+            </SuiBox>
+          </Card>
+        </SuiBox>
+      </SuiBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Privilegios;
