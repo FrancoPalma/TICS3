7c3dd153986b2285662bb6eedabf777c4e4441ab
joaquin <joaquin.vial@mail.udp.cl>
2021-09-29 13:25:22 -0300

    asd

--- Backend/src/config/database.sql
+++ Backend/src/config/database.sql
@@ -7,6 +7,53 @@ especialidad varchar(50),
 password text
 );
 
+create table privilegios (
+    rut_usuario numeric(8,0),
+    gestion_usuario BOOLEAN,
+    gestion_ficha BOOLEAN,
+    gestion_priv BOOLEAN,
+    gestion_evaluacion BOOLEAN,
+    gestion_infante BOOLEAN,
+    CONSTRAINT fk_rut FOREIGN KEY(rut_usuario) REFERENCES usuario(rut)
+);
+
+create table horario (
+    rut_usuario numeric(8,0),
+    descripcion DATE,
+    fecha DATE,
+    hora TIME,
+    sala VARCHAR(30),
+    CONSTRAINT fk_rut FOREIGN KEY(rut_usuario) REFERENCES usuario(rut)
+);
+
+create table informe (
+    id INTEGER PRIMARY KEY,
+    rut_infante VARCHAR(10),
+    metodologia TEXT,
+    CONSTRAINT fk_infante FOREIGN KEY(rut_infante) REFERENCES infante(rut)
+);
+
+create table evaluacion (
+    id INTEGER PRIMARY KEY,
+    id_informe INTEGER,
+    nombre TEXT,
+    CONSTRAINT fk_informe FOREIGN KEY(id_informe) REFERENCES informe(id)
+);
+
+create table objetivo (
+    id INTEGER PRIMARY KEY,
+    id_informe INTEGER,
+    nombre TEXT,
+    CONSTRAINT fk_informe FOREIGN KEY(id_informe) REFERENCES informe(id)
+);
+
+create table analisis (
+    id_informe INTEGER,
+    conclusion TEXT,
+    recomendacion TEXT,
+    CONSTRAINT fk_informe FOREIGN KEY(id_informe) REFERENCES informe(id)
+);
+
 insert into usuario(rut,nombre,telefono,email,especialidad, password)
 values (1,'franco',56123456789,'hola@gmail.com','fonoaudiologo', 'hola');
 
