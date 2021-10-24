87d208e5140b4c656c94c6c133857a93f0e0d1a6
joaquin <joaquin.vial@mail.udp.cl>
2021-10-24 18:08:07 -0300

    asd

--- Backend/src/controllers/usuarioController.js
+++ Backend/src/controllers/usuarioController.js
@@ -18,7 +18,7 @@ usuarioController.postEditarPrivilegios = (req, res) => {
 	let gestion_evaluacion = req.body.gestion_evaluacion;
 	let gestion_infante = req.body.gestion_infante;
 
-	pool.query('INSERT INTO privilegios(gestion_usuario, gestion_ficha, gestion_priv, gestion_evaluacion, gestion_infante) VALUES ($1,$2,$3,$4,$5) WHERE privilegios.rut_usuario = $6', [gestion_usuario, gestion_ficha, gestion_priv, gestion_evaluacion, gestion_infante, rut_usuario], (err) => {
+	pool.query('UPDATE privilegios SET gestion_usuario = $1, gestion_ficha = $2, gestion_priv = $3, gestion_evaluacion = $4, gestion_infante = $5 WHERE privilegios.rut_usuario = $6', [gestion_usuario, gestion_ficha, gestion_priv, gestion_evaluacion, gestion_infante, rut_usuario], (err) => {
 		if(err){res.sendStatus(404)}
 		return res.sendStatus(200);
 	});
