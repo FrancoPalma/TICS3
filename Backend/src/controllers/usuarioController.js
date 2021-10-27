060add88ead004d8c4e5b06f0b8749f4c3731270
joaquin <joaquin.vial@mail.udp.cl>
2021-10-27 17:41:58 -0300

    ad

--- Backend/src/controllers/usuarioController.js
+++ Backend/src/controllers/usuarioController.js
@@ -9,6 +9,15 @@ usuarioController.getVerPrivilegios = (req, res) => {
 	});
 };
 
+usuarioController.getVerDatos = (req, res) => {
+	let id_jardin = req.user.id_jardin;
+
+	pool.query('SELECT usuario.rut, usuario.nombre, usuario.telefono, usuario.email, usuario.especialidad FROM usuario, jardin WHERE usuario.rut = privilegios.rut_usuario AND usuario.id_jardin = $1', [id_jardin], (err, result) => {
+		if(err){ return res.sendStatus(404)}
+		return res.json(result.rows)
+	})
+};
+
 usuarioController.postEditarPrivilegios = (req, res) => {
 
 	let rut_usuario = req.params.rut_usuario;
--- Backend/src/routes/usuario.js
+++ Backend/src/routes/usuario.js
@@ -5,6 +5,8 @@ const sesionController = require('../controllers/sesionController.js')
 
 router.get('/ver_privilegios', usuarioController.getVerPrivilegios);
 
+router.get('/ver_datos', usuarioController.getVerDatos);
+
 router.post('/editar_privilegios/:rut_usuario', usuarioController.postEditarPrivilegios);
 
 router.post('/eliminar_usuario/:rut_usuario', usuarioController.postEliminarUsuario)
