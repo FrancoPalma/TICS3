7ec21876deacbc183b3483154e7183c143e1f7df
joaquin <joaquin.vial@mail.udp.cl>
2021-11-06 19:30:42 -0300

    asd

--- Backend/src/controllers/informeController.js
+++ Backend/src/controllers/informeController.js
@@ -8,35 +8,53 @@ informeController = {}
 informeController.postGuardarInforme = async (req, res) => {
   let rut_infante = req.body.rut_infante;
   let contenido = req.body.contenido;
-  await pool.query('BEGIN', async (err) => {
-    if(err){return res.sendStatus(404)}
-    await pool.query('INSERT INTO informe(rut_infante, rut_usuario) VALUES ($1, $2) RETURNING id', (err, result) => {
+  let id_informe = req.body.id_informe;
+
+  if(id_informe == 0){
+    await pool.query('BEGIN', async (err) => {
       if(err){return res.sendStatus(404)}
-      
-      fs.writeFile(__dirname, '../public/informes/informe' + result.rows[0].id + '.pdf', contenido, (err) => {
-        if (err) {
-          pool.query('ROLLBACK')
-          return res.sendStatus(404)
-        }
-        htmlToPdf.convertHTMLString(contenido, path.join(__dirname, '../public/informes/informe' + result.rows[0].id + '.pdf'), async (err) => {
+      await pool.query('INSERT INTO informe(rut_infante, rut_usuario) VALUES ($1, $2) RETURNING id', (err, result) => {
+        if(err){return res.sendStatus(404)}
+        id_informe = result.rows[0].id
+        fs.writeFile(__dirname, '../public/informes/informe' + id_informe + '.pdf', contenido, (err) => {
           if (err) {
             pool.query('ROLLBACK')
             return res.sendStatus(404)
           }
-          await pool.query('COMMIT', (err) => {
-            if(err){return res.sendStatus(404)}
-            return res.sendStatus(200);
-          })
+          htmlToPdf.convertHTMLString(contenido, path.join(__dirname, '../public/informes/informe' + id_informe+ '.pdf'), async (err) => {
+            if (err) {
+              pool.query('ROLLBACK')
+              return res.sendStatus(404)
+            }
+            await pool.query('COMMIT', (err) => {
+              if(err){return res.sendStatus(404)}
+              return res.json({
+                  id: id_informe
+                }
+                );
+            })
 
-        });
+          });
+        })
       })
     })
-  })
+  }else{
+    fs.writeFile(__dirname, '../public/informes/informe' + id_informe + '.pdf', contenido, (err) => {
+      if (err) {return res.sendStatus(404)}
+      htmlToPdf.convertHTMLString(contenido, path.join(__dirname, '../public/informes/informe' + id_informe+ '.pdf'), async (err) => {
+        if (err) {return res.sendStatus(404)}
+        return res.json({
+            id: id_informe
+          }
+        );
+      })
+    });
+  }
 };
 
 informeController.getVerInforme = async (req, res) => {
-  let rut_infante = req.body.rut_infante;
-  fs.readFile(path.join(__dirname, '../public/informes/informe'+ rut_infante +'.pdf') , function (err,data){
+  let id_informe = req.body.id_informe;
+  fs.readFile(path.join(__dirname, '../public/informes/informe'+ id_informe +'.pdf') , function (err,data){
     if(err){return res.sendStatus(404);}
     res.contentType("application/pdf");
     res.send(data);
