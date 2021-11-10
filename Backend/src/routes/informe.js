910ea9ed2467aeb45c055b1482d4b998b1f93eb3
joaquin <joaquin.vial@mail.udp.cl>
2021-11-10 14:22:33 -0300

    asd

--- Backend/src/routes/informe.js
+++ Backend/src/routes/informe.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const informeController = require('../controllers/informeController.js');
 const sesionController = require('../controllers/sesionController.js');
 
-router.post('/guardar_informe', sesionController.isLoggedIn, sesionController.gestionEvaluacion, informeController.postGuardarInforme);
+router.post('/guardar_informe', informeController.postGuardarInforme);
 
 router.post('/ver_informe', informeController.postVerInforme);
 
-router.post('/eliminar_informe', sesionController.isLoggedIn, sesionController.gestionEvaluacion, informeController.postEliminarInforme);
+router.post('/eliminar_informe', informeController.postEliminarInforme);
 
-router.post('/editar_informe', sesionController.isLoggedIn, sesionController.gestionEvaluacion, informeController.postEditarInforme);
+router.post('/editar_informe', informeController.postEditarInforme);
 
 module.exports = router;
