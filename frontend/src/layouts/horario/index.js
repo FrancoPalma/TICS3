b994cf1fdbc42f5325eedcde4991b32373e1412a
Franco Palma <fpalmatrejo@gmail.com>
2021-11-20 17:58:41 -0300

    asd

--- frontend/src/layouts/horario/index.js
+++ frontend/src/layouts/horario/index.js
@@ -1,99 +1,106 @@
-import * as React from 'react';
+import React from 'react';
+import Scheduler, { Resource } from 'devextreme-react/scheduler';
+
 import { useState } from "react";
 import Card from "@material-ui/core/Card";
-import { styled } from '@mui/material/styles';
-import Grid from "@material-ui/core/Grid";
-import Icon from "@material-ui/core/Icon";
-
-// Soft UI Dashboard Material-UI components
 import SuiBox from "components/SuiBox";
 import SuiTypography from "components/SuiTypography";
-
-// Soft UI Dashboard Material-UI example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 import Footer from "examples/Footer";
-import MiniStatisticsCard from "examples/Cards/StatisticsCards/MiniStatisticsCard";
-import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
-import GradientLineChart from "examples/Charts/LineCharts/GradientLineChart";
-
-// Soft UI Dashboard Material-UI base styles
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import styles from "layouts/tables/styles";
+import Table from "examples/Table";
+import SuiButton from "components/SuiButton";
+import Icon from "@material-ui/core/Icon";
 import typography from "assets/theme/base/typography";
+import { Confirm,} from 'react-st-modal';
+import { useHistory } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+/*npm install @mui/material @emotion/react @emotion/styled*/
+//npm install devextreme@21.2 devextreme-react@21.2 --save --save-exact
+//npx -p devextreme-cli devextreme add devextreme-react
+import { employees, data } from './data.js';
+import DataCell from './DataCell.js';
+import ResourceCell from './ResourceCell.js';
 
-// Dashboard layout components
-import BuildByDevelopers from "layouts/dashboard/components/BuildByDevelopers";
-import WorkWithTheRockets from "layouts/dashboard/components/WorkWithTheRockets";
-import Projects from "layouts/dashboard/components/Projects";
-import OrderOverview from "layouts/dashboard/components/OrderOverview";
-
-// Data
-import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
-import gradientLineChartData from "layouts/dashboard/data/gradientLineChartData";
-import Paper from '@material-ui/core/Paper';
-import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
-import {
-    Scheduler,
-    MonthView,
-    Appointments,
-    Toolbar,
-    DateNavigator,
-    AppointmentTooltip,
-    AppointmentForm,
-    EditRecurrenceMenu,
-    Resources,
-    DragDropProvider,
-    ViewSwitcher,
-    DayView,
-    WeekView,
-    TodayButton,
-    AllDayPanel,
-  ConfirmationDialog
-} from '@devexpress/dx-react-scheduler-material-ui';
-
-let info = JSON.parse(localStorage.getItem('usuario'));
-console.log(info)
-const schedulerData = [
-  { startDate: '2021-11-01T09:45', endDate: '2021-11-01T11:00', title: 'Meeting' },
-  { startDate: '2021-11-01T12:00', endDate: '2021-11-01T13:30', title: 'Go to a gym' },
-];
+const currentDate = new Date(2021, 5, 2, 11, 30);
+const groups = ['employeeID'];
+const views = ['month'];
 
 
 
-export default function Horario () {
-
-
-  
-return(
-<DashboardLayout>
-<DashboardNavbar />
-  <Paper>
-    <Scheduler
-    height={660}
-    data={schedulerData}
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
     >
-      <ViewState
-        currentDate="2021-11-01"
-        defaultCurrentViewName="Month"
-      />
-      <DayView/>
-      <WeekView/>
-      <EditingState/>
-      <MonthView/>
-      
-      <Toolbar/>
-      <ViewSwitcher/>
-      <Appointments/>
-      <Resources/>
-      <DateNavigator />
-      <TodayButton />
-          <EditRecurrenceMenu />
-          <AppointmentTooltip
-
-          />
-          <AppointmentForm />
-          <DragDropProvider />
-    </Scheduler>
-  </Paper>
-</DashboardLayout>
-);
+      {value === index && (
+        <SuiBox p={3}>
+          <SuiTypography>{children}</SuiTypography>
+        </SuiBox>
+      )}
+    </div>
+  );
 }
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function Horario() {
+  const hist = useHistory();
+  const classes = styles();
+  const [tabValue, setTabValue] = useState(0);
+  const handleSetTabValue = (event, newValue) => setTabValue(newValue);
+  const [Listo, setListo] = useState(0);
+
+  return (
+    <DashboardLayout>
+    <DashboardNavbar/>
+      <SuiBox py={6}>
+        <SuiBox mb={6}>
+          <Card>
+            <Scheduler
+              timeZone="America/Los_Angeles"
+              dataSource={data}
+              dataCellComponent={DataCell}
+              resourceCellComponent={ResourceCell}
+              groups={groups}
+              views={views}
+              defaultCurrentView="month"
+              defaultCurrentDate={currentDate}
+              height={600}
+              showAllDayPanel={true}
+              firstDayOfWeek={1}
+              startDayHour={8}
+              endDayHour={18}
+            >
+              <Resource
+                label="Employee"
+                fieldExpr="employeeID"
+                dataSource={employees}
+                allowMultiple={false}
+              />
+            </Scheduler>
+          </Card>
+        </SuiBox>
+        <Card>
+        </Card>
+      </SuiBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
