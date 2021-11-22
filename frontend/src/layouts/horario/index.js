2af2f36a73376412ecf9e1c31050f730d99d1860
Franco Palma <fpalmatrejo@gmail.com>
2021-11-22 19:28:58 -0300

    asd

--- frontend/src/layouts/horario/index.js
+++ frontend/src/layouts/horario/index.js
@@ -11,6 +11,7 @@ import styles from "layouts/tables/styles";
 import Table from "examples/Table";
 import { useHistory } from "react-router-dom";
 import SuiButton from "components/SuiButton";
+import Icon from "@material-ui/core/Icon";
 
 /*npm install @mui/material @emotion/react @emotion/styled*/
 
@@ -60,7 +61,13 @@ export default function Horario() {
   const [tabValue, setTabValue] = useState(0);
   const handleSetTabValue = (event, newValue) => setTabValue(newValue);
   const [Listo, setListo] = useState(0);
-  const [row, setRow] = useState([]);
+  const [rows, setRow] = useState([]);
+  const columns = [
+    { name: "inicio", align: "left" },
+    { name: "fin", align: "left" },
+    { name: "descripcion", align: "center" },
+    { name: "sala", align: "center" }
+  ];
   const [selectedDay, setSelectedDay] = useState(undefined);
   function handleDayClick(day, { selected }) {
     if (selected) {
@@ -72,6 +79,35 @@ export default function Horario() {
   function EnviarFecha(){
     if(selectedDay != undefined){
       let aux = selectedDay.toLocaleDateString()
+      let dia;
+      let mes;
+      let ano;
+      while(rows.length > 0) {
+        rows.pop();
+      }
+      if (aux[1]=='/'){
+        //D/MM/AAAA
+        dia= '0'+aux[0];
+        if(aux[3]=='/'){
+          mes = '0'+aux[2];
+          ano = aux[4]+aux[5]+aux[6]+aux[7]; 
+        }else{
+          mes = aux[2]+aux[3];
+          ano = aux[5]+aux[6]+aux[7]+aux[8];
+        }
+
+      }else{
+        //DD/MM/AAAA
+        dia= aux[0]+aux[1];
+        if(aux[4]=='/'){
+          mes = '0'+aux[3];
+          ano = aux[5]+aux[6]+aux[7]+aux[8];
+        }else{
+          mes = aux[3]+aux[4];
+          ano = aux[6]+aux[7]+aux[8]+aux[9];
+        }
+      }
+      console.log(ano+'-'+mes+'-'+dia)
       fetch('/horario/ver_horario',{
         method: 'POST',
         headers: {
@@ -79,24 +115,43 @@ export default function Horario() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          fecha: aux[6]+aux[7]+aux[8]+aux[8]+aux[5]+aux[3]+aux[4]+aux[2]+aux[0]+aux[1]
+          fecha: ano+'-'+mes+'-'+dia
         })
       })
       .then((response) => {
-        if(response.status !== 404){
+        if(response.status == 200){
           console.log("ok")
-          /*setListo(1)*/
           return response.json()
         }else{
           console.log("error")
         }
       })
       .then(users => {
-        console.log(users);
+        for(let i=0; i < users.length;i++){
+          let aux = true;
+          for(let e=0;e < rows.length;e++){
+            if(users[i].rut == rows[e].rut){
+              aux=false;
+            }
+          }
+          if(aux == true){
+            rows.push({id:users[i].id,
+              descripcion: users[i].descripcion,
+              inicio: users[i].inicio,
+              fin: users[i].fin,
+              sala: users[i].sala
+            })
+          }
+        }
+        if(rows.length > 0)
+        setListo(1)
+        else
+        alert("No hay nada asignado")
       })
       .catch((error) => {
         console.log(error)
       });
+  
     }else{
       alert("Seleccione una fecha")
     }
@@ -113,7 +168,7 @@ export default function Horario() {
             months={MONTHS}
             selectedDays={selectedDay}
             weekdaysShort={WEEKDAYS_SHORT}
-            disabledDays={{ daysOfWeek: [0,6] }}
+            disabledDays={{ daysOfWeek: [0] }}
             firstDayOfWeek={1}/>
              {selectedDay ? (
           <p>You clicked {selectedDay.toLocaleDateString()}</p>
@@ -138,6 +193,12 @@ export default function Horario() {
         <SuiBox py={6}>
           <SuiBox mb={6}>
             <Card>
+            <SuiButton  buttonColor="info" iconOnly onClick = {async() => {setListo(0)}}>
+              <Icon classsName="material-icons-round">keyboard_backspace</Icon>
+            </SuiButton>
+            <SuiBox customClass={classes.tables_table}>
+              <Table columns={columns} rows={rows} />
+            </SuiBox>
             </Card>
           </SuiBox>
         </SuiBox>
